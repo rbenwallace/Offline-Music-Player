4bf0b580de78a6575486e58f05de5a0e39953701
Ben Wallace <rbenwallace@gmail.com>
2022-03-16 00:59:57 -0400

    Adding contents for main file

--- Shared/Offline_Music_PlayerApp.swift
+++ Shared/Offline_Music_PlayerApp.swift
@@ -6,10 +6,27 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct Offline_Music_PlayerApp: App {
     let persistenceController = PersistenceController.shared
+    
+    init(){
+        do {
+            let viewContext = persistenceController.container.viewContext
+            let request = NSFetchRequest<Playlist>(entityName: "Playlist")
+            let result = try viewContext.fetch(request) as [Playlist]
+            if result.count < 1 {
+                let mostPlayed = Playlist(context: viewContext)
+                mostPlayed.timestamp = Date()
+                mostPlayed.title = "Most Played"
+                try viewContext.save()
+            }
+        } catch let error as NSError {
+            print("Error: \(error.localizedDescription)")
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
