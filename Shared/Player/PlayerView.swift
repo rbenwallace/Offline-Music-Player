3eda25dfbae9b2d54982d30b2b5253bd10891652
Ben Wallace <rbenwallace@gmail.com>
2022-04-07 01:09:58 -0400

    Redesigned PlayerView

--- Shared/Player/PlayerView.swift
+++ Shared/Player/PlayerView.swift
@@ -1,142 +1,219 @@
 //
 //  PlayerView.swift
-//  Offline Cloud Music Player
+//  Offline Music Player (iOS)
 //
-//  Created by Ben Wallace on 2022-02-26.
+//  This view class represents the full screen player view that is displayed when a song is playing and the user clicks on the minimized bar view of the current playing song
 //
 
 import SwiftUI
 import AVKit
 
 struct PlayerView: View {
-    private enum PlaybackState: Int {
-        case waitingForSelection
-        case buffering
-        case playing
-    }
-    
+    // environment object which contains published variables used in this view, and allows for audio player manipulation
     @EnvironmentObject var model: Model
-    public var songId: UUID
-    public var fromPlaylist: Bool
-    public var songTitle: String
     
+    // State TimeInterval variable which is updated by the view's receiver and populates the time slider's current time for the current playing song
     @State private var currentTime: TimeInterval = 0
+    
+    // State TimeInterval variable which is updated by the view's receiver and populates the time slider's duration time for the current playing song
     @State private var currentDuration: TimeInterval = 0
-    @State private var state = PlaybackState.waitingForSelection
     
     var body: some View {
-        VStack {
-            Slider(value: $currentTime,
-                   in: 0...currentDuration,
-                   onEditingChanged: sliderEditingChanged,
-                   minimumValueLabel: Text("\(TimeFormat.formatSecondsToHMS(currentTime))"),
-                   maximumValueLabel: Text("\(TimeFormat.formatSecondsToHMS(currentDuration))")) {
-                    // I have no idea in what scenario this View is shown...
-                    Text("seek/progress slider")
-            }
-            .disabled(state != .playing)
-            .padding(.bottom, 30)
-            
+        // only displays the view if there is a song currently playing
+        if self.model.currentSong != nil{
             HStack {
-                Image(systemName: "gobackward.15")
-                    .font(.system(size: 25))
-                    .padding(.trailing)
-                    .onTapGesture {
-                        model.goBackward()
-                 }
+                Spacer(minLength: 0)
                 
-                Image(systemName: "backward.fill")
-                    .font(.system(size: 25))
-                    .padding(.trailing)
-                    .onTapGesture {
-                        model.prev()
-                        model.isPlaying = true
-                 }
-                
-                Image(systemName: (model.isPlaying && model.audioPlayer.currentItem != nil) ? "pause.circle.fill": "play.circle.fill")
-                    .font(.system(size: 25))
-                    .padding(.trailing)
-                    .onTapGesture {
-                        model.isPlaying.toggle()
-                        if model.isPlaying{
-                            model.playSong(id: songId, fromPlaylist: fromPlaylist, songTitle: songTitle)
-                        } else {
-                            model.pause()
+                VStack {
+                    // represents the image in the upper center of the view
+                    Image(uiImage: UIImage(named: "song_cover") ?? UIImage())
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding()
+                        .padding(.top, 40)
+                    
+                    // represents the song title and song artist of the current song playing
+                    VStack {
+                        Text(self.model.currentSong!)
+                            .foregroundColor(.white)
+                            .font(Font.system(.title2).bold())
+                            .lineLimit(1)
+                        Text("Unknown Artist")
+                            .foregroundColor(.gray)
+                            .font(Font.system(.title3).bold())
+                    }
+                    .padding(.bottom, 20)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                    
+                    Spacer(minLength: 0)
+                    
+                    // represents the time bar slider for users to manipulate the current time of the current song playing
+                    Slider(value: self.$currentTime,
+                           in: 0...self.currentDuration,
+                           onEditingChanged: timeSliderUpdated,
+                           minimumValueLabel: Text("\(Helper.formattedTime(self.currentTime))"),
+                           maximumValueLabel: Text("\(Helper.formattedTime(self.currentDuration))")) {
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 20)
+                    
+                    Spacer(minLength: 0)
+                    
+                    HStack {
+                        // represents the previous buttton
+                        Image(systemName: "backward.fill")
+                            .font(.system(size: 30))
+                            .padding(.horizontal, 20)
+                            .multilineTextAlignment(.center)
+                            .onTapGesture {
+                                self.model.prev()
+                                self.model.isPlaying = true
+                            }
+                        
+                        // represents the play/pause buttton
+                        Image(systemName: (model.isPlaying && (model.audioPlayer.currentItem != nil)) ? "pause.fill": "play.fill")
+                            .font(.system(size: 40))
+                            .padding()
+                            .padding(.horizontal, 20)
+                            .multilineTextAlignment(.center)
+                            .onTapGesture {
+                                if self.model.isPlaying{
+                                    self.model.pause()
+                                } else {
+                                    self.model.unPause()
+                                }
+                         }
+                        
+                        // represents the next buttton
+                        Image(systemName: "forward.fill")
+                            .font(.system(size: 30))
+                            .padding(.horizontal, 20)
+                            .multilineTextAlignment(.center)
+                            .onTapGesture {
+                                self.model.next()
+                                self.model.isPlaying = true
+                            }
+                    }
+                    .padding(.bottom, 20)
+                    .foregroundColor(.white)
+                    
+                    Spacer(minLength: 0)
+                    
+                    HStack {
+                        // represents the buttton to exit the full screen view
+                        Image(systemName: "arrow.backward.to.line")
+                            .font(.system(size: 25))
+                            .padding(.horizontal, 15)
+                            .multilineTextAlignment(.center)
+                            .onTapGesture {
+                                withAnimation(Animation.spring(response: 0.7, dampingFraction: 0.85)) {
+                                    model.isPlayerViewPresented = false
+                                }
+                            }
+                        
+                        Spacer(minLength: 0)
+                        
+                        // represents the -15 seconds buttton
+                        Image(systemName: "gobackward.15")
+                            .font(.system(size: 25))
+                            .padding(.horizontal, 15)
+                            .multilineTextAlignment(.center)
+                            .onTapGesture {
+                                self.model.goBackward()
+                            }
+                        
+                        Spacer(minLength: 0)
+                        
+                        // represents the +15 seconds buttton
+                        Image(systemName: "goforward.15")
+                            .font(.system(size: 25))
+                            .padding(.horizontal, 15)
+                            .multilineTextAlignment(.center)
+                            .onTapGesture {
+                                self.model.goForward()
+                            }
+                        
+                        Spacer(minLength: 0)
+                        
+                        // represents the sleep timer menu buttton
+                        Menu {
+                            Button(action: { }, label: { Text("Cancel") })
+                            Button(action: { model.stopTimer() }, label: { Text("Stop Timer") })
+                            Button(action: { model.sleepTimer(time: 3600) }, label: { Text("1 Hour") })
+                            Button(action: { model.sleepTimer(time: 1800) }, label: { Text("30 Minutes") })
+                            Button(action: { model.sleepTimer(time: 600) }, label: { Text("10 Minutes") })
+                            Button(action: { model.sleepTimer(time: 60) }, label: { Text("1 Minute") })
+                        } label: {
+                            model.sleepTimerOn ? Label("", systemImage: "moon.fill")
+                                .tint(.white)
+                                .font(.system(size: 25))
+                                .padding(.horizontal, 15)
+                                .multilineTextAlignment(.center)
+                            : Label("", systemImage: "moon")
+                                .tint(.white)
+                                .font(.system(size: 25))
+                                .padding(.horizontal, 15)
+                                .multilineTextAlignment(.center)
                         }
-                 }
-                
-                Image(systemName: "forward.fill")
-                    .font(.system(size: 25))
-                    .padding(.trailing)
-                    .onTapGesture {
-                        model.next()
-                        model.isPlaying = true
-                 }
-                
-                Image(systemName: "goforward.15")
-                    .font(.system(size: 25))
-                    .padding(.trailing)
-                    .onTapGesture {
-                        model.goForward()
-                 }
-                
-                Menu {
-                    Button(action: { }, label: { Text("Cancel") })
-                    Button(action: { model.stopTimer() }, label: { Text("Stop Timer") })
-                    Button(action: { model.sleepTimer(time: 3600) }, label: { Text("1 Hour") })
-                    Button(action: { model.sleepTimer(time: 1800) }, label: { Text("30 Minutes") })
-                    Button(action: { model.sleepTimer(time: 600) }, label: { Text("10 Minutes") })
-                    Button(action: { model.sleepTimer(time: 60) }, label: { Text("1 Minute") })
-                } label: {
-                    model.sleepTimerOn ? Label("", systemImage: "moon.fill").tint(.white) : Label("", systemImage: "moon").tint(.white)
+                    }
+                    .padding(.bottom, 20)
+                    .foregroundColor(.white)
+                    
+                    Spacer(minLength: 0)
                 }
+                // Listen out for the time observer publishing changes to the player's time
+                .onReceive(model.timeObserver.publisher) { time in
+                    // Update the local var
+                    self.currentTime = time
+                }
+                // Listen out for the item observer publishing a change to whether the player has an item
+                .onReceive(model.currentSongObserver.publisher) { hasItem in
+                    self.currentTime = 0
+                    if hasItem && self.model.audioPlayer.currentItem != nil {
+                        if self.model.audioPlayer.currentItem!.duration.isNumeric {
+                            self.currentDuration =  TimeInterval(self.model.audioPlayer.currentItem!.duration.seconds)
+                        }
+                    } else {
+                        self.currentDuration =  0
+                    }
+                }
+                Spacer(minLength: 0)
             }
-        }
-        .onAppear(perform: loadData)
-        .padding()
-        // Listen out for the time observer publishing changes to the player's time
-        .onReceive(model.timeObserver.publisher) { time in
-            // Update the local var
-            self.currentTime = time
-            // And flag that we've started playback
-            if time > 0 {
-                self.state = .playing
-            }
-        }
-        // Listen out for the duration observer publishing changes to the player's item duration
-        .onReceive(model.durationObserver.publisher) { duration in
-            // Update the local var
-            self.currentDuration = duration
-        }
-        // Listen out for the item observer publishing a change to whether the player has an item
-        .onReceive(model.itemObserver.publisher) { hasItem in
-            self.state = hasItem ? .buffering : .waitingForSelection
-            self.currentTime = 0
-            self.currentDuration = 0
+            .onAppear(perform: loadData)
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color(.systemPink), .black]), startPoint: .top, endPoint: .bottom)
+            )
+            .edgesIgnoringSafeArea(.all)
         }
     }
     
+    // actions that occur when the view is presented to the user
     private func loadData(){
-        self.currentDuration = model.audioPlayer.currentItem?.duration.seconds ?? 0
+        if self.model.audioPlayer.currentItem != nil {
+            // populates the time bar slider's duration with the duration of the current playing song
+            if self.model.audioPlayer.currentItem!.duration.isNumeric {
+                self.currentDuration =  TimeInterval(self.model.audioPlayer.currentItem!.duration.seconds)
+            }
+            
+            // populates the time bar slider's current time with the current playback time of the current playing song
+            if self.model.audioPlayer.currentItem!.currentTime().isNumeric {
+                self.currentTime =  TimeInterval(self.model.audioPlayer.currentItem!.currentTime().seconds)
+            }
+        }
     }
     
-    // MARK: Private functions
-    private func sliderEditingChanged(editingStarted: Bool) {
-        if editingStarted {
-            // Tell the PlayerTimeObserver to stop publishing updates while the user is interacting
-            // with the slider (otherwise it would keep jumping from where they've moved it to, back
-            // to where the player is currently at)
-            model.timeObserver.pause(true)
+    private func timeSliderUpdated(updateStarted: Bool) {
+        if updateStarted {
+            // Informs the timeObserver that the time slider is being updated and to temporarily stop sending time updates
+            self.model.timeObserver.setTimeUpdating(timeUpdating: true)
         }
         else {
-            // Editing finished, start the seek
-            state = .buffering
-            let targetTime = CMTime(seconds: currentTime,
-                                    preferredTimescale: 600)
-            model.audioPlayer.seek(to: targetTime) { _ in
-                // Now the (async) seek is completed, resume normal operation
-                model.timeObserver.pause(false)
-                self.state = .playing
+            // The time slider update is complete and the audio player must now seek to the new current time
+            self.model.audioPlayer.seek(to: CMTime(seconds: currentTime, preferredTimescale: 600)) { _ in
+                // Informs the timeObserver that the time slider is done updating and to continue sending time updates
+                self.model.timeObserver.setTimeUpdating(timeUpdating: false)
             }
         }
     }
